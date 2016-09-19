839ecfecd6a9a48274b0fcfedd3fb6ea6a7c249c
Nguyen Huu Giap <nguyenhuugiap@gmail.com>
2016-09-19 22:56:20 +0700

    test send mail heroku 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,16 +18,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'enigmatic-beach-19769.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['giapnguyenhuu4@gmail.com'],
-    :password       => ENV['huugiap09'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["giapnguyenhuu4@gmail.com"],
+  password: ENV["huugiap09"]
+}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
